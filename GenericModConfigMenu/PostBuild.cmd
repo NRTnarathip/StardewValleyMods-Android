ea3e69bd1e9c27e42dfcd4a25f0721f4fe8ad4d4
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2025-11-07 20:07:59 +0700

    Update: GMCM

---
+++ GenericModConfigMenu/PostBuild.cmd
@@ -0,0 +1,12 @@
+set AppName=abc.smapi.gameloader
+
+adb shell am force-stop %AppName%
+
+if errorlevel 1 (
+    echo ADB is not connected.
+    exit /b
+)
+
+adb push "bin/Release/GenericModConfigMenu.dll" "/storage/emulated/0/Android/data/%AppName%/files/Mods/GenericModConfigMenu"
+
+adb shell am start -n %AppName%"/crc64e91f1276c636690c.LauncherActivity" --ez "IsClickStartGame" true
